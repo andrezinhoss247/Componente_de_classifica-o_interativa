5876b765cd50274b303d1e14e1bedbb67acea5e1
André Silva <andrezinhoss247@gmail.com>
2025-05-13 00:29:38 -0300

    adicionado comandos no script

--- javaScript/script.js
+++ javaScript/script.js
@@ -1,1 +1,48 @@
 
+const btn = [...window.document.querySelectorAll('button.btn')]
+const btnEnviar = window.document.getElementById('btnEnviar')
+
+let resultado 
+
+for (const i of btn) {
+
+    
+    i.addEventListener("click", function (){
+
+        for(const j of btn){
+            j.classList.remove('clicar')
+             
+        }
+        i.classList.add('clicar')
+         
+         
+        resultado = i.innerHTML
+
+          
+})
+
+}
+btnEnviar.addEventListener("click", function () {
+
+    window.location.href = "../html/obrigado.html"
+})
+
+
+
+
+        
+
+    
+
+
+
+        
+        
+   
+        
+    
+   
+
+   
+
+

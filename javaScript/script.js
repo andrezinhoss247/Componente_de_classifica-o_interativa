f0ebdc34e4f3b42983122ec9ababe1479f3d9c8c
André Silva <andrezinhoss247@gmail.com>
2025-05-14 00:20:05 -0300

    scrit funcionado

--- javaScript/script.js
+++ javaScript/script.js
@@ -1,31 +1,45 @@
 
-const btn = [...window.document.querySelectorAll('button.btn')]
-const btnEnviar = window.document.getElementById('btnEnviar')
+// variavel recebe elementos button com class btn 
+const btn = [...window.document.querySelectorAll("button.btn")]
 
-let resultado 
+//varivael recebe elemento com Id btnEnviar
+const btnEnviar = window.document.querySelector("button#btnEnviar")
+
 
 for (const i of btn) {
 
+    let res = null
     
     i.addEventListener("click", function (){
 
+        //for para varrer e eliminar class clicar
         for(const j of btn){
             j.classList.remove('clicar')
              
         }
-        i.classList.add('clicar')
-         
-         
-        resultado = i.innerHTML
 
-          
+        //adicionando class click em um elemento 
+        i.classList.add('clicar')
+        res = i.textContent
+        sessionStorage.setItem('resultado', res)
+       
+        
 })
 
 }
-btnEnviar.addEventListener("click", function () {
 
+
+//Envento para direcionar pagina
+btnEnviar.addEventListener("click", function () {
+   
     window.location.href = "./html/obrigado.html"
+    const suaNota = window.document.getElementById('suaNota')
+    suaNota.innerHTML = sessionStorage.getItem('resultado')
+     
+
+
 })
+
 
 
 
